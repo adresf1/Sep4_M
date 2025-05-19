b6fbbdb64a216d454aea3f9eb2e71770330f7a48
HiroshikunDK <36033304+HiroshikunDK@users.noreply.github.com>
2025-05-19 02:54:22 +0200

    Updated from Warning and added new dummy connection string

--- DataProcessing/db.py
+++ DataProcessing/db.py
@@ -4,7 +4,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL") or DATABASE_URL = 'postgresql://dummy_url'
+
+if DATABASE_URL == 'postgresql://dummy_url':
+    print("Application have defaulted to dummy connection string")
 
 engine = create_engine(
     DATABASE_URL,
